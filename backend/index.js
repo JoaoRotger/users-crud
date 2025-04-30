a8a1e5d54c8deb5e827d69ec4723639f1e5e73a8
João <93404889+JoaoRotger@users.noreply.github.com>
2025-04-30 15:06:00 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const pool = require('./db');
 const usersRouter = require('./routes/users');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
